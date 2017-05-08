5faa4ce46fb6066cddbb3592867ad7862d0ba3eb
David Jones <drj@pobox.com>
2017-05-08 13:55:44 +0100

    Remove single queue analysis

--- bin/qtotal.py
+++ bin/qtotal.py
@@ -14,24 +14,6 @@ whereas that man page uses 1-based indexing.
 class Statistic:
     """Structure to hold accumulated and computed statistics."""
 
-def queue_rows():
-    """
-    An iterator that yields each row of the accounting file that
-    matches the queue.
-    Each row is split at each ':' and yielded as a list.
-    """
-
-    accounting_file = os.environ.get('ACCOUNTING',
-      'hidelab.accounting')
-    queue = os.environ.get('QUEUE', 'hidelab.q')
-
-    with open(accounting_file) as inp:
-        for line in inp:
-            row = line.split(':')
-            if row[:1] != [queue]:
-                continue
-            yield row
-
 def all_rows():
     accounting_file = os.environ.get('ACCOUNTING',
       'hidelab.accounting')
@@ -42,32 +24,6 @@ def all_rows():
             yield row
 
 def main():
-    cpu_total = sum(
-        float(row[13]) * float(row[34]) for row in queue_rows())
-
-    mem_total = sum(float(row[37]) for row in queue_rows())
-
-    first = float('inf')
-    last = -float('inf')
-    for row in queue_rows():
-        if float(row[9]) == 0.0:
-            continue
-        first = min(first, float(row[9]))
-        last = max(last, float(row[10]))
-    elapsed = last - first
-    print("cpu total", cpu_total)
-    print("mem total", mem_total)
-    print("Total elapsed time in analysis period", elapsed)
-    print()
-
-    print("mean cores utilised", cpu_total/elapsed)
-    print("mean mem", mem_total/elapsed)
-    print()
-
-    wait_time = dict()
-    vmem = dict()
-    cpu = dict()
-
     # This sorts by queue name (because that's the first element).
     rows = sorted(all_rows())
     # Remove rows that do not have valid wait times
